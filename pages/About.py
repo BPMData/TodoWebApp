c359633ab2e7424e874a12169e72a431ebb509ea
BPMData <bryan.patrick.a.murphy@gmail.com>
2023-06-19 21:34:03 -0400

    Some experiments creating a multi-page Web app.

---
+++ pages/About.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.set_page_config(layout="wide", page_title="About This App")
+
+st.title("This To-Do App was:")
+st.subheader("Written in Python using Streamlit on:")
+st.write("06/19/2023, following along with S. Ardit's Python Mega-Course.")
